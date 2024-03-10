5f7e20cda9c99653621e3d6ac7039188e4aa1bf2
Meik Behr <meik.behr@gmx.de>
2024-03-10 17:37:09 +0100

    tidy up

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 function init () {
 	renderHeader();
 	renderFooter();
@@ -7,39 +8,59 @@ function init () {
 }
 
 
-function renderHeader() {
-	const container = document.getElementById('header');
+/**
+ * Give ID as String into this function so it will search your document for this ID, clear ist and returns the result
+ * @param {string} ID 
+ */
+function getContainerForHtmlAndClearIt(ID) {
+	const container = document.getElementById(ID);
 	container.innerHTML = '';
+	return container;
+}
+
+
+/**
+ * This function is used to render the HTML for the Header-Element of the document
+ */
+function renderHeader() {
+	const container = getContainerForHtmlAndClearIt('header');
 	container.innerHTML += headerHTML();
 };
 
 
+/**
+ * This function is used to render the HTML for the Footer-Element of the document
+ */
 function renderFooter() {
-	const container = document.getElementById('footer');
-	container.innerHTML = '';
+	const container = getContainerForHtmlAndClearIt('footer');
 	container.innerHTML += footerHTML();
 };
 
 
-
-
+/**
+ * This function is used to render the HTML for the Overlay-Element of the document, make it visible and disallow scrolling for all other elements
+ */
 function showContactOverlay() {
+	const container = getContainerForHtmlAndClearIt('contact__container');
+	container.innerHTML += contactHTML();
 	document.getElementById('contact__overlay').classList.remove('d-none');
 	document.body.classList.add('no-scroll');
-	const container = document.getElementById('contact__container');
-	container.innerHTML = '';
-	container.innerHTML += contactHTML();
 }
 
 
-
+/**
+ * This function starts setting the Eventlistner for closing the Overlay using ESC (Keydown) oder Click on X
+ */
 function setEventListener() {
 	const close = document.getElementById(`contact__overlay__close`);
 	closeOverlay(close);
 }
 
 
-
+/**
+ * This function is used to close the overlay if ESC is keydown or the X is clicked. It calls hideOverlay()
+ * @param {HTMLElement } close 
+ */
 function closeOverlay(close) {
 	close.addEventListener('click', hideOverlay);
 	document.addEventListener('keydown', (event) => {
@@ -50,6 +71,9 @@ function closeOverlay(close) {
 }
 
 
+/**
+ * This function will "close" the overlay by setting .d-none and give the re-permission to scroll the content
+ */
 function hideOverlay() {
 	const card_overlay = document.getElementById('contact__overlay');
 	card_overlay.classList.add('d-none');
