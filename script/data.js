c419d08632ff9dbfcf061e8d4568c60195454574
Meik Behr <meik.behr@gmx.de>
2024-03-13 16:25:52 +0100

    started data.js - one recipe for training online

--- script/data.js
+++ script/data.js
@@ -1,3 +1,24 @@
 "use strict";
 
 
+recipes = [
+
+    {
+        "name": "Griechischer Bauernsalat",
+        "author": "Anton",
+        "author_img": "",
+        "zeit": "15 Min.",
+        "gesamtzeit": "15 Min.",
+        "level": "simple",
+        "datum": "28.09.2020",
+        "portionen": 4,
+        "zutaten": ["Salatgurken", "Paprikaschoten", "Tomaten", "Zwiebeln", "Glas Oliven, schwar, 100g", "Salz und Pfeffer", "Zitrone", "Olivenöl", "Oregano"],
+        "menge": ["1", "2", "500 g", "2", "200 g", "1", "", "1", "125 ml", ""],
+        "zubereitung": "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    },
+
+    2,
+
+    3
+
+]
