9dea7473ee21b62da67ed8fd5671d5e8a6c62356
Meik Behr <meik.behr@gmx.de>
2024-03-15 21:33:09 +0100

    added additonal recipe.html for Anton, Meik & Oliver

--- script/render.js
+++ script/render.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 function headHTML() {
     return /*html*/ `
     <meta charset="UTF-8">
@@ -7,18 +8,14 @@ function headHTML() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="robots" content="noindex, nofollow">
 		<meta name="description" content="This is a trainingswebside of the Developer Akademie Group 107">
-
         <link rel="icon" type="image" href="./img/logo_small.png" />
-
         <link rel="stylesheet" type="text/css" href="./style/style.css">
 		<link rel="stylesheet" type="text/css" href="./style/content.css">
 		<link rel="stylesheet" type="text/css" href="./style/contact.css">
 		<link rel="stylesheet" type="text/css" href="./style/recipe.css">
 		<link rel="stylesheet" type="text/css" href="./style/mediaqueries.css">
-
         <script defer src="./script/render.js" type="text/javascript"></script>
         <script defer src="./script/script.js" type="text/javascript"></script>
-
     <title>Kochwelt - Gruppe 107</title>	
     `;
 }
@@ -31,7 +28,6 @@ function headerHTML() {
 			<img class="header__logo" src="./img/logo.png" alt="Logo Kochwelt: Eine Kochmütze">
 		</a>
 	</figure>
-
 	<nav class="header__nav">
 		<div>
 			<a href="./index.html">Start</a>
@@ -59,16 +55,12 @@ function footerHTML() {
 			</a>
 		</figure>
 	</section>
-
-
 	<nav class="footer__nav">
-
 		<section class="footer__nav__legals">
 			<a href="#">Impressum</a>
 			<p>|</p>
 			<a href="#">Datenschutz</a>
 		</section>
-
 		<section class="footer__nav_socialMedia">
 			<a href="https://www.facebook.com/?lang=de">
 				<div class="footer__nav__facebook footer__nav_socialMedia__icons" role="img" alt="Logo Facebook"></div>
@@ -77,7 +69,6 @@ function footerHTML() {
 			<div class="footer__nav__twitter footer__nav_socialMedia__icons" role="img" alt="Logo Twitter / X"></div>
 			</a>
 		</section>
-
 	</nav>
     `;
 }
@@ -85,14 +76,10 @@ function footerHTML() {
 
 function contactHTML() {
     return /*html*/ `
-
 		<a id="content__close" href="#" onclick="hideOverlay()"></a>
-
         <div class="contact__content">
-
 			<h3 class="contact__heading">Sie können uns gern kontaktieren:</h3>
 			<p class="contact__paragraph">Haben Sie Fragen zu einem Rezept, möchten ein eignes einreichen oder uns aus anderen Gründen kontaktieren, können Sie uns gern jederzeit über dieses Formular erreichen.</p>
-
 			<form  class="form" onsubmit="sendMail(event)">
 				<div class="form__horizontal">
 					<div class="form__group">
@@ -104,19 +91,15 @@ function contactHTML() {
 						<label class="form__label" for="contact-email">E-mail</label>
 					</div>
 				</div>
-
                 <div class="form__horizontal">
 					<div class="form__group__textarea">
 						<textarea name="message" class="form__textarea" id="contact-message" required></textarea>
 						<label class="form__label" for="contact-message">Nachricht</label>
 					</div>
 				</div>
-
-
 				<div class="contact__submit-container">
 					<input type="submit" class="button--contact" value="Anfrage abschicken">
 				</div>
-				
 			</form>
         </div>
     `;
