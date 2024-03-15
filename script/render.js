22c9728bef879f18fb2e7eeb7422d3edd65edff2
Meik Behr <meik.behr@gmx.de>
2024-03-15 21:15:34 +0100

    shrinking files together, reducing js-files, putting everything into 2

---
+++ script/render.js
@@ -0,0 +1,123 @@
+"use strict";
+
+function headHTML() {
+    return /*html*/ `
+    <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="robots" content="noindex, nofollow">
+		<meta name="description" content="This is a trainingswebside of the Developer Akademie Group 107">
+
+        <link rel="icon" type="image" href="./img/logo_small.png" />
+
+        <link rel="stylesheet" type="text/css" href="./style/style.css">
+		<link rel="stylesheet" type="text/css" href="./style/content.css">
+		<link rel="stylesheet" type="text/css" href="./style/contact.css">
+		<link rel="stylesheet" type="text/css" href="./style/recipe.css">
+		<link rel="stylesheet" type="text/css" href="./style/mediaqueries.css">
+
+        <script defer src="./script/render.js" type="text/javascript"></script>
+        <script defer src="./script/script.js" type="text/javascript"></script>
+
+    <title>Kochwelt - Gruppe 107</title>	
+    `;
+}
+
+
+function headerHTML() {
+    return /*html*/ `
+    <figure class="header__logo__container">
+		<a href="./index.html">
+			<img class="header__logo" src="./img/logo.png" alt="Logo Kochwelt: Eine Kochmütze">
+		</a>
+	</figure>
+
+	<nav class="header__nav">
+		<div>
+			<a href="./index.html">Start</a>
+		</div>
+		<div>
+			<a href="./recipe.html">Rezept des Tages</a>
+		</div>
+		<div>
+			<a href="#" id="linkToContactForm" onclick="showContactOverlay()">Kontakt</a>
+		</div>
+		<div>
+			<a href="#">Impressum</a>
+		</div>
+	</nav>
+    `;
+}
+
+
+function footerHTML() {
+    return /*html*/ `
+    <section class="footer__logo">
+		<figure>
+			<a href="./index.html">
+				<img src="./img/logo.png" alt="">
+			</a>
+		</figure>
+	</section>
+
+
+	<nav class="footer__nav">
+
+		<section class="footer__nav__legals">
+			<a href="#">Impressum</a>
+			<p>|</p>
+			<a href="#">Datenschutz</a>
+		</section>
+
+		<section class="footer__nav_socialMedia">
+			<a href="https://www.facebook.com/?lang=de">
+				<div class="footer__nav__facebook footer__nav_socialMedia__icons" role="img" alt="Logo Facebook"></div>
+			</a>
+			<a href="https://twitter.com/?lang=de">
+			<div class="footer__nav__twitter footer__nav_socialMedia__icons" role="img" alt="Logo Twitter / X"></div>
+			</a>
+		</section>
+
+	</nav>
+    `;
+}
+
+
+function contactHTML() {
+    return /*html*/ `
+
+		<a id="content__close" href="#" onclick="hideOverlay()"></a>
+
+        <div class="contact__content">
+
+			<h3 class="contact__heading">Sie können uns gern kontaktieren:</h3>
+			<p class="contact__paragraph">Haben Sie Fragen zu einem Rezept, möchten ein eignes einreichen oder uns aus anderen Gründen kontaktieren, können Sie uns gern jederzeit über dieses Formular erreichen.</p>
+
+			<form  class="form" onsubmit="sendMail(event)">
+				<div class="form__horizontal">
+					<div class="form__group">
+						<input name="name" type="text" class="form__input" id="contact-name" placeholder="Name" required>
+						<label class="form__label" for="contact-name">Name</label>
+					</div>
+					<div class="form__group">
+						<input  name="email" type="email" class="form__input" id="contact-email" placeholder="E-mail" size="30" required>
+						<label class="form__label" for="contact-email">E-mail</label>
+					</div>
+				</div>
+
+                <div class="form__horizontal">
+					<div class="form__group__textarea">
+						<textarea name="message" class="form__textarea" id="contact-message" required></textarea>
+						<label class="form__label" for="contact-message">Nachricht</label>
+					</div>
+				</div>
+
+
+				<div class="contact__submit-container">
+					<input type="submit" class="button--contact" value="Anfrage abschicken">
+				</div>
+				
+			</form>
+        </div>
+    `;
+}
--- script/script.js
+++ script/script.js
@@ -99,7 +99,6 @@ function hideOverlay() {
 function sendMail(event){﻿
     event.preventDefault();
     const data = new FormData(event.target);
-
     fetch("https://formspree.io/f/mjvnebjr", {
         method: "POST",
         body: new FormData(event.target),
@@ -113,3 +112,40 @@ function sendMail(event){﻿
     });
 }
 
+
+/**
+ * This function checks, if the value of the inputfield is between 1 and 20. If so, it calculates the new amount of 
+ * ingredients  and updates the data-original-amount with the value from the input field for further calculations.
+ */
+function updateIngredients() {
+    const newPortionsValue = parseFloat(document.getElementById("recipe__input").value);
+    if (newPortionsValue > 0 && newPortionsValue <= 20) {
+        const actualAmounts = document.querySelectorAll('.recipe__table__amount');
+        const table = document.querySelector('.recipe__table');
+        const originalAmount = parseFloat(table.getAttribute('data-original-amount'));
+        actualAmounts.forEach((element) => {
+            if (element.innerHTML !== '') {
+                const amount = parseFloat(element.innerHTML);
+                const unit = element.innerHTML.split(" ").slice(1).join(" ");
+                const amountNew = amount * newPortionsValue / originalAmount;
+                const newAmountWithUnits = amountNew + " "  + unit;
+                element.innerHTML = newAmountWithUnits;
+            }
+        })
+        table.setAttribute('data-original-amount', `${newPortionsValue}`);
+    }
+}
+
+
+/**
+ * This function sets an Eventlistner to the inputfield to capture any keydown events. If Enter is pressed, the updateIngredients function will be called
+ */
+function setEventListenerRecipe() {
+    const inputField = document.getElementById('recipe__input');
+    inputField.addEventListener('keydown', (event) => {
+		if (event.key === "Enter") {
+            console.log('ENTER');
+			updateIngredients();
+		}
+	});
+}
